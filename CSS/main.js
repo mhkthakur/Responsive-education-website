4a59411788d4d728c2ced9610bcb677514b4f850
Mehak Thakur <mhkthakur>
2024-04-29 23:09:35 +0530

    Website completed

--- CSS/main.js
+++ CSS/main.js
@@ -16,7 +16,7 @@ faqs.forEach(faq => {
 
         // change icon
         const icon = faq.querySelector('.faq__icon i');
-        if(icon.className === 'uil uil-plus'){
+        if (icon.className === 'uil uil-plus') {
             icon.className = "uil uil-minus";
         } else {
             icon.className = "uil uil-plus";
